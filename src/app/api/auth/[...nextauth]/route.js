a86e5aa6bfd2ed127ecd9e8f978dfb60a7506207
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-28 13:47:16 +0700

    Create RegisterPage and its functionality

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,45 @@
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { MongoDBAdapter } from '@auth/mongodb-adapter'
+import * as mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+
+import clientPromise from '@/lib/mongoConnect'
+import { User } from '@/models/User'
+
+export const authOptions = {
+	secret: process.env.SECRET,
+	adapter: MongoDBAdapter(clientPromise),
+	providers: [
+		CredentialsProvider({
+			name: 'Credentials',
+			id: 'credentials',
+			credentials: {
+				username: {
+					label: 'Email',
+					type: 'email',
+					placeholder: 'test@example.com',
+				},
+				password: { label: 'Password', type: 'password' },
+			},
+			async authorize(credentials, req) {
+				const email = credentials?.email
+				const password = credentials?.password
+
+				mongoose.connect(process.env.MONGO_URL)
+				const user = await User.findOne({ email })
+				const passwordOk = user && bcrypt.compareSync(password, user.password)
+
+				if (passwordOk) {
+					return user
+				}
+
+				return null
+			},
+		}),
+	],
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
